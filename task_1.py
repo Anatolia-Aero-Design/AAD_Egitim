9146bb8fc99a5e424a6fff437634623b66e61b6d
dodo <diyar20002010@gmail.com>
2021-11-05 16:58:42 +0100

    ilk task

---
+++ task_1.py
@@ -0,0 +1,138 @@
+##Kullanıcıdan aldığınız 3 tane sayıyı çarparak ekrana yazdırın. Ekrana yazdırma işlemini format metoduyla yapmaya çalışın
+
+
+
+
+#1) Kullanıcıdan ad,soyad ve numara bilgisini alarak bunları alt alta ekrana yazdırın.
+
+
+
+
+#2) Kullanıcıdan bir dik üçgenin dik olan iki kenarını(a,b) alın ve hipotenüs uzunluğunu bulmaya çalışın. Hipotenüs Formülü: a^2 + b^2 = c^2
+
+
+
+"""
+3) Kullanıcıdan alınan boy ve kilo değerlerine göre beden kitle indeksini hesaplayın ve şu kurallara göre ekrana şu yazıları yazdırın.
+#Beden Kitle İndeksi: Kilo / Boy(m) * Boy(m)
+
+#BKİ 18.5'un altındaysa -------> Zayıf
+
+#BKİ 18.5 ile 25 arasındaysa ------> Normal
+
+#BKİ 25 ile 30 arasındaysa --------> Fazla Kilolu
+
+#BKİ 30'un üstündeyse -------------> Obez"""
+
+
+
+"""
+4) Kullanıcının girdiği vize1,vize2,final notlarına notlarına göre harf notunu hesaplayın.
+Vize1 toplam notun %30'una etki edecek.
+
+Vize2 toplam notun %30'una etki edecek.
+
+Final toplam notun %40'ına etki edecek.
+
+
+Toplam Not >=  90 -----> AA
+
+Toplam Not >=  85 -----> BA
+
+Toplam Not >=  80 -----> BB
+
+Toplam Not >=  75 -----> CB
+
+Toplam Not >=  70 -----> CC
+
+Toplam Not >=  65 -----> DC
+
+Toplam Not >=  60 -----> DD
+
+Toplam Not >=  55 -----> FD
+
+Toplam Not <  55 -----> FF
+
+
+"""
+
+
+
+
+
+
+
+
+"""
+5)
+1'den 10'kadar olan sayılarla ekrana çarpım tablosu bastırmaya çalışın.
+İpucu: İç içe 2 tane for döngüsü kullanın. Aynı zamanda sayıları range() fonksiyonunu kullanarak elde edin.
+"""
+
+
+
+
+
+
+
+
+
+"""
+6)
+Her bir while döngüsünde kullanıcıdan bir sayı alın ve kullanıcının girdiği sayıları "toplam" isimli bir değişkene ekleyin. Kullanıcı "q" tuşuna bastığı zaman döngüyü sonlandırın ve ekrana "toplam değişkenini" bastırın.
+İpucu : while döngüsünü sonsuz koşulla başlatın ve kullanıcı q'ya basarsa döngüyü break ile sonlandırın.
+
+"""
+
+
+
+
+
+
+
+
+
+"""
+7) 
+Kullanıcıdan aldığınız bir sayının mükemmel olup olmadığını bulmaya çalışın.
+Bir sayının kendi hariç bölenlerinin toplamı kendine eşitse bu sayıya "mükemmel sayı" denir. Örnek olarak, 6 mükemmel bir sayıdır. (1 + 2 + 3 = 6)
+"""
+
+
+
+
+
+
+
+
+"""
+8) 
+Kullanıcıdan 2 tane sayı alarak bu sayıların en büyük ortak bölenini (EBOB) dönen bir tane fonksiyon yazın.
+
+"""
+
+
+
+
+
+"""
+9)
+Kullanıcıdan 2 tane sayı alarak bu sayıların en küçük ortak katlarını (EKOK) dönen bir tane fonksiyon yazın.
+
+"""
+
+
+
+"""
+10)
+
+Bu projede ise 4 tane sınıfı oluşturun.
+Hayvan Sınıfı ------> Bütün hayvanların ortak özelliklerinin toplandığı sınıf olacak.
+
+Köpek Sınıfı ------> Bu sınıf, hayvan sınıfından miras alan bir sınıf olacak. Ayrıca bu sınıfa köpeklere ait ek özellikler ve metodlar ekleyin.
+
+Kuş Sınıfı ------> Bu sınıf, hayvan sınıfından miras alan bir sınıf olacak. Ayrıca bu sınıfa kuşlara ait ek özellikler ve metodlar ekleyin.
+
+At Sınıfı ------> Bu sınıf, hayvan sınıfından miras alan bir sınıf olacak. Ayrıca bu sınıfa atlara ait ek özellikler ve metodlar ekleyin.
+
+"""
