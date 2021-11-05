d1ea993909572391a96bce6ded4c4a571fde01fa
dodo <diyar20002010@gmail.com>
2021-11-05 23:34:49 +0100

    task2

---
+++ task_2.py
@@ -0,0 +1,53 @@
+"""
+1.
+Create a deck of cards class. Internally, the deck of cards should use another class, a card class. Your requirements are:
+
+-The Deck class should have a deal method to deal a single card from the deck
+After a card is dealt, it is removed from the deck.
+-There should be a shuffle method which makes sure the deck of cards has all 52 cards and then rearranges them randomly.
+-The Card class should have a suit (Hearts, Diamonds, Clubs, Spades) and a value (A,2,3,4,5,6,7,8,9,10,J,Q,K)
+"""
+
+"""
+*2.
+basit bir drone classı yaratın her dronun sahip olması gerek standart özellikler:
+
+*id,
+*x,y,z kordinatları
+*hızı,
+*taşıyabiliceği yük,
+*batarya,
+her dronun sahip olması gereken fonksiyonlar(Bu fonksiyonlar hayali bir drone için calışmalı mesela takeoff komutunu verdiğinizde ucağın kalkmasını değil sadece y değerinin takeoff komutunda verdiğiniz yüksekliğe eşitlenmesini istiyorum, aynı şekilde (10,5,20) noktasına git dediğinizde gercekten oraya giden bir drone deil xyz si bu birimlere eşitlenmesini istiyorum),
+
+*takeoff
+*return to launch
+*move
+*yük al
+*yük bırak,
+*kalkış noktasına uzaklık,
+"""
+
+
+"""
+*3.
+1000 int den oluşan random bir liste yaratın ve istediğiniz sorting algoritmasını kullanarak küçükten büyüğe sıralayan bir script yazınız.
+"""
+
+
+
+
+
+"""
+*4.
+n katlı bir gökdelende olduğunuzu farz edin, elinizde 2 adet yumurta var. amacınız yumurtanın kırılmadan atılabileceği en yüksek katı bulmak. yumurtayı atmak için k adet deneme yapabilirsiniz ve yumurta herhangi bir katta kırılabilir (evet herhangi bir kat)
+
+n verilen k değerine göre en fazla kaç olabilir?
+"""
+
+
+
+"""
+*5.
+bir sayının cift olup olmadığını hesaplayan en iyi fonksiyonu yazmaya calışın. (diyer katılımcılarla karşılaştırılacak)
+
+"""
